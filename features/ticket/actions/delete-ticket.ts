0f4e2b858bce542f5723ad0300338aea8365db0b
Roman Parkhomenko <roman.s.parkhomenko@gmail.com>
2026-09-02 17:01:49 +0300

    add revalidatePath on ticket delete to test deploy

--- features/ticket/actions/delete-ticket.ts
+++ features/ticket/actions/delete-ticket.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { ticketsPath } from "@/lib/paths";
 import { prisma } from "@/lib/prisma";
@@ -7,6 +8,7 @@ import { prisma } from "@/lib/prisma";
 const deleteTicket = async (ticketId: string) => {
   await prisma.ticket.delete({ where: { id: ticketId } });
 
+  revalidatePath(ticketsPath);
   redirect(ticketsPath);
 };
 
